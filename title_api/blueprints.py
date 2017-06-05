e78ebefe3eea446557dedf7b5a704d95e09da2f2
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2017-06-05 18:27:54 +0100

    version url prefix

--- title_api/blueprints.py
+++ title_api/blueprints.py
@@ -5,7 +5,7 @@ from title_api.views import general, title_v1
 def register_blueprints(app):
     """Adds all blueprint objects into the app."""
     app.register_blueprint(general.general)
-    app.register_blueprint(title_v1.title_v1)
+    app.register_blueprint(title_v1.title_v1, url_prefix='/v1')
 
     # All done!
     app.logger.info("Blueprints registered")
